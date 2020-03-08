4b495e44b53e58750df281afc119db0d85af48ce
zr123 <25320035+zr123@users.noreply.github.com>
2020-03-08 04:26:24 +0100

    Refactor for Part 2

--- lib/AoC2019/Day02.pm
+++ lib/AoC2019/Day02.pm
@@ -43,16 +43,22 @@ sub runInstructions {
 	return encode(@instructions)
 }
 
+sub run_with {
+	my ($noun, $verb, $instriction_string) = @_;
+	$instriction_string =~ s/^(\d+),\d+,\d+/$1,$noun,$verb/;
+	print($instriction_string);
+	$instriction_string = runInstructions($instriction_string);
+	my @instructions = decode($instriction_string);
+	return $instructions[0];
+}
+
 sub part1 {
 	my ($instriction_string) = @_;
 	my @instructions = decode($instriction_string);
-	$instructions[1] = 12;
-	$instructions[2] = 2;
-	$instriction_string = encode(@instructions);
-	$instriction_string = runInstructions($instriction_string);
-	@instructions = decode($instriction_string);
-	return $instructions[0];
+	return run_with(12, 2, $instriction_string);
 }
+
+#run_with(12, 2, "1,9,10,3,2,3,11,0,99,30,40,50");
 
 
 1;
